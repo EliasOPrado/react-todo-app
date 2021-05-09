57fd69cd385c8ae8861d6a7eba5e4b545e351d12
Oliveira Prado <39608843+EliasOPrado@users.noreply.github.com>
2021-05-09 01:42:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,11 +22,28 @@ export default function App() {
     const NewTodos = [...todos, { text }];
     setTodos(NewTodos);
   };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
   return (
     <div className="App">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
